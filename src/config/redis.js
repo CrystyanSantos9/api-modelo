0742a5c6dc3b731d0950ae5a17a897c456fc5e4e
CrystyanSantos9 <cryssantos9@gmail.com>
2022-02-03 13:21:40 +0000

    acertando dockerfile para cors funcionar

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,6 +1,6 @@
 import "dotenv/config"
 
 export default {
-    host: process.env.NODE_ENV==='production' ? process.env.REDIS_HOST: 'localhost',
+    host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
 }
